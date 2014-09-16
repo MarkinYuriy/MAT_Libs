cb5d25965343790907b59b5caed080e1ffd6b01a
Yuri <yuri@inetex.co.il>
2014-09-16 16:13:52 +0300

    New FesBes2 Interface

--- src/src/mat/IFesBes2.java
+++ src/src/mat/IFesBes2.java
@@ -1,8 +1,18 @@
 package mat;
 
 public interface IFesBes2 {
-	String [] getContacts(String username, String[] socialNames);//assuming Google
-	boolean shareByMail(String urlMatt, String[] contacts, String userName, String socialName);
-	String [] getAuthorizedSocialNames(String username);
-	boolean setToken(String username, String socialName,String current, String refresh);
+	 /* *********************************************** */
+    //PROTOCOL CONSTANTS
+    //social networks names
+    public static final String GOOGLE = "google";
+    public static final String APPLE = "apple";
+    public static final String TWITTER = "twitter";
+    public static final String FACEBOOK = "facebook";
+    public static final String WINDOWS = "windows";
+
+    String[] getContacts(String username, String[] socialNames);//assuming Google
+    boolean shareByMail(String urlMatt, String[] contacts, String userName, String socialName);
+
+    String[] getAuthorizedSocialNames(String username);
+    boolean setToken(String username, String socialName, String current, String refresh);
 }
