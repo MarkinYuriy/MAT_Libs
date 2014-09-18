f4638736c5b6c668ab5129c8be0ec265456957aa
Yuri <yuri@inetex.co.il>
2014-09-18 15:22:44 +0300

    Replace interface name IBes1Bes2 with IBackConnector

---
+++ src/src/mat/IBackConnector.java
@@ -0,0 +1,11 @@
+package mat;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface IBackConnector {
+	List<Boolean> getSlots(String username, String [] snNames,
+			mat.MattData interval) throws IOException;
+	void setMatCalendar(String username,String [] snNames,List<mat.Matt> matts );
+	
+}
