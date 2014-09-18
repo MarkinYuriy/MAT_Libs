92b54bde2d703e1e5e686e98a948d3d21ac15812
Yuri <yuri@inetex.co.il>
2014-09-18 15:19:01 +0300

    Added throws exception

--- src/src/mat/IBes1Bes2.java
+++ src/src/mat/IBes1Bes2.java
@@ -1,9 +1,11 @@
 package mat;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IBes1Bes2 {
-	List<Boolean> getSlots(String username, String [] snNames, mat.MattData interval);
+	List<Boolean> getSlots(String username, String [] snNames,
+			mat.MattData interval) throws IOException;
 	void setMatCalendar(String username,String [] snNames,List<mat.Matt> matts );
 	
 }
