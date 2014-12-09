03d0e1e5d5210f52a70028c437dabe19b954c4d9
demoniskatel <demoniskatel@gmail.com>
2014-12-09 12:20:18 +0200

    deleted snNames from functions
    snNames in Matt now

--- src/src/mat/IBackConnector.java
+++ src/src/mat/IBackConnector.java
@@ -1,11 +1,8 @@
 package mat;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface IBackConnector {
-	List<Boolean> getSlots(String username, String [] snNames,
-			mat.MattData interval) ;
-	void setMatCalendar(String username,String [] snNames,List<mat.Matt> matts );
-	
+	List<Boolean> getSlots(String username, mat.MattData interval) ;
+	void setMatCalendar(String username, List<mat.Matt> matts);
 }
