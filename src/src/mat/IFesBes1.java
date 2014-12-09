dbe9406891d11e8d09786e92164c240c2583041c
demoniskatel <demoniskatel@gmail.com>
2014-12-09 12:27:38 +0200

    mattName was exchanged to mattId in IFesBes1

--- src/src/mat/IFesBes1.java
+++ src/src/mat/IFesBes1.java
@@ -6,9 +6,9 @@ public interface IFesBes1 {
 	int matLogin(String username, String password);
 	String [] getMattNames(String username);
 	mat.Matt createMatt(mat.MattData data, String username);
-	mat.Matt getMatt(String mattName, String username);
-	boolean saveMatt(mat.Matt mattOld,mat.Matt mattNew,String username );
-	boolean removeMatt(String mattName, String username);
+	mat.Matt getMatt(int mattId, String username);
+	boolean saveMatt(mat.Matt mattOld,mat.Matt mattNew,String username);
+	boolean removeMatt(int mattId);
 	mat.Person getProfile(String username);
 	void setActive(String username,String hashcode);
 	void updateMatCalendarInSN(String username, String snName);
