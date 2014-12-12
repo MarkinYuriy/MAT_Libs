0cb6faeab5abf798bbbaa6d2b886271920ec71a0
demoniskatel <demoniskatel@gmail.com>
2014-12-12 23:11:23 +0200

    uploadMatt was added into IBackConnector

--- src/src/mat/IBackConnector.java
+++ src/src/mat/IBackConnector.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface IBackConnector {
 	List<Boolean> getSlots(String username, mat.MattData interval) ;
 	void setMatCalendar(String username, List<mat.Matt> matts);
+	void uploadMatt(String username, mat.Matt matt);
 }
