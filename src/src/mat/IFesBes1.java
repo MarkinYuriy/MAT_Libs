d129c673c6baeda18bf870f6ce1079f379b3dfa7
demoniskatel <demoniskatel@gmail.com>
2014-12-16 17:25:00 +0200

    getMatNames return HashMap<Integer, String>

--- src/src/mat/IFesBes1.java
+++ src/src/mat/IFesBes1.java
@@ -1,11 +1,13 @@
 package mat;
 
+import java.util.HashMap;
+
 public interface IFesBes1 {
 	int setProfile(mat.Person person);
 	int updateProfile(mat.Person person);
 	int matLogin(String userName, String password);
-	String [] getMattNames(String userName);
-	mat.Matt getMatt(int matId);
+	HashMap<Integer,String> getMattNames(String userName);
+	mat.Matt getMatt(int mattId);
 	int saveMatt(mat.Matt mattNew,String userName);//return mattId
 	boolean removeMatt(int mattId);
 	mat.Person getProfile(String userName);
